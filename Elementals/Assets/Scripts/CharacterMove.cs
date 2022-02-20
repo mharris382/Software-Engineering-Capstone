05b4c9becf2951fdb54fa68d128bd99fc44888e6
Max Harris <mharris382@gmail.com>
2022-02-20 18:40:38 -0500

    applied data oriented principles to movement script

--- Elementals/Assets/Scripts/CharacterMove.cs
+++ Elementals/Assets/Scripts/CharacterMove.cs
@@ -3,43 +3,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class InputState
+{
+    public Vector2 MoveInput { get; set; }
+    public bool Jump { get; set; }
+}
 
+public class MovementState
+{
+    public Vector2 Velocity { get; set; }
+    public bool IsGrounded { get; set; }
+    
+    public float HorizontalMovement
+    {
+        get => Velocity.x;
+        set => Velocity = new Vector2(value, Velocity.y);
+    }
+}
+
+
+[RequireComponent(typeof(CharacterState))]
 public class CharacterMove : MonoBehaviour
 {
     private Rigidbody2D _rb;
+    private CharacterState _state;
     public float moveSpeed = 10f;
     public float jumpVert = 10f;
     public LayerMask groundMask;
-    
-    public float HorizontalMovement { get; set; }
-    public bool isGrounded;
 
-    // Start is called before the first frame update
+    
+    
+
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _state = GetComponent<CharacterState>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        HorizontalMovement = Input.GetAxis("Horizontal") * moveSpeed;
+        _state.Movement.HorizontalMovement = _state.Input.MoveInput.x * moveSpeed;
         var hit = Physics2D.Raycast(transform.position, Vector2.down, distance: 0.5f, groundMask);
-        isGrounded = hit;
+        _state.Movement.IsGrounded = hit;
 
-        if (isGrounded) 
+        if (_state.Movement.IsGrounded && _state.Input.Jump) 
         {
-            if (Input.GetButtonDown("Jump"))
-            {
-                _rb.AddForce(Vector2.up * jumpVert, ForceMode2D.Impulse);
-            }
+            _rb.AddForce(Vector2.up * jumpVert, ForceMode2D.Impulse);
         }
     }
 
     // Based on Physics Updates
     private void FixedUpdate()
     {
-        _rb.velocity = new Vector2(HorizontalMovement, _rb.velocity.y);
+        _rb.velocity = _state.Movement.Velocity;
     }
 
 }
---
+++ Elementals/Assets/Scripts/CharacterState.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CharacterState : MonoBehaviour
+{
+    public InputState Input { get; private set; }
+    public MovementState Movement { get; private set; }
+
+
+    private void Awake()
+    {
+        Input = new InputState();
+        Movement = new MovementState();
+    }
+}
