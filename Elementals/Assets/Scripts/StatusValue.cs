d29b148cb052f302d27e6e0ac28ec84524d0f02f
dkohn <douglasjkohn@gmail.com>
2022-02-20 19:03:00 -0600

    Added Health bar functionality.

---
+++ Elementals/Assets/Scripts/StatusValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class StatusValue : MonoBehaviour
+{
+    [SerializeField]
+    private float maxValue = 10;
+    private float currentValue = 10;
+    public float MaxValue => maxValue;
+    public float CurrentValue => currentValue;
+    
+    [Range(-5, 5)]
+    public float Generation;
+    private void Awake()
+    {
+        currentValue = maxValue;
+    }
+
+    private void Update()
+    {
+        currentValue += Generation * Time.deltaTime;
+        currentValue = Mathf.Clamp(currentValue, 0, maxValue);
+    }
+}
---
+++ Elementals/Assets/Scripts/UIstatusBar.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIstatusBar : MonoBehaviour
+{
+    public Image uiProgressBar;
+    public StatusValue target;
+
+    private void Update()
+    {
+        uiProgressBar.fillAmount = target.CurrentValue / target.MaxValue;
+    }
+}
