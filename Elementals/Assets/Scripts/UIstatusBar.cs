3002873201eb69095e1c9aa1c85c5d3605963cb7
dkohn <douglasjkohn@gmail.com>
2022-02-22 18:11:14 -0600

    Moving Platform Added, mana bar working.
    Added a moving platform, changed how jump works, mana bar moving now.

--- Elementals/Assets/Scripts/UIstatusBar.cs
+++ Elementals/Assets/Scripts/UIstatusBar.cs
@@ -4,14 +4,14 @@ using UnityEngine.UI;
 public class UIstatusBar : MonoBehaviour
 {
     public Image uiProgressBar;
-    public Image uiManaProgressBar;
+    // public Image uiManaProgressBar;
     public StatusValue target;
-    public StatusValue manaTarget;
+    // public StatusValue manaTarget;
     
     
     private void Update()
     {
         uiProgressBar.fillAmount = target.CurrentValue / target.MaxValue;
-        uiManaProgressBar.fillAmount = manaTarget.CurrentValue / manaTarget.MaxValue;
+        // uiManaProgressBar.fillAmount = manaTarget.CurrentValue / manaTarget.MaxValue;
     }
 }
