35f32f90742970fa56f7464f7d51e46a133ddc22
Max Harris <mharris382@gmail.com>
2022-02-27 14:11:03 -0500

    added force pull to mana gatherer

--- Elementals/Assets/Scripts/ManaGatherer.cs
+++ Elementals/Assets/Scripts/ManaGatherer.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class ManaGatherer : MonoBehaviour
 {
+   [SerializeField] private float consumeRadius = 1;
+   [SerializeField] private float forceStrength = 5;
    public IManaFinder Finder { get; set; }
    public ElementContainer element;
    private ManaState _mana;
@@ -19,7 +21,32 @@ public class ManaGatherer : MonoBehaviour
       if (_state.Gathering)
       {
          var nearbyMana = Finder.GetManaNearby(element.Element);
-         
+         foreach (var mana in nearbyMana)
+         {
+            //TODO: check if we are close enough to pick it up
+            
+            //TODO: otherwise force pull it
+         }
       }
+   }
+
+
+   private void Consume(Mana mana)
+   {
+      //TODO: add mana to state
+      Destroy(mana.gameObject);
+   }
+   private void ForcePullMana(Mana mana)
+   {
+      var rb = mana.GetComponent<Rigidbody2D>();
+      var dir = ((Vector2) transform.position - rb.position).normalized;
+      var force = dir * forceStrength;
+      rb.AddForce(force);
+   }
+
+   private bool IsInsideRadius(Mana mana, float radius)
+   {
+      var dist = Vector2.Distance(mana.transform.position, transform.position);
+      return dist < radius;
    }
 }
