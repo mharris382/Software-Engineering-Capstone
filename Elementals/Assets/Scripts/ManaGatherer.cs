399f55a245b9f4a8cd482593965546ff8149242d
dkohn <douglasjkohn@gmail.com>
2022-02-27 13:06:15 -0600

    Working on element mana gathering.

--- Elementals/Assets/Scripts/ManaGatherer.cs
+++ Elementals/Assets/Scripts/ManaGatherer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,20 @@ using UnityEngine;
 public class ManaGatherer : MonoBehaviour
 {
    public IManaFinder Finder { get; set; }
+   public ElementContainer element;
+   private ManaState _mana;
+   private CasterState _state;
+
+   private void Awake()
+   {
+      Finder = GetComponent<IManaFinder>();
+      _mana = GetComponentInChildren<ManaState>();  
+      _state = GetComponent<CasterState>();
+
+      if (_state.Gathering)
+      {
+         var nearbyMana = Finder.GetManaNearby(element.Element);
+         
+      }
+   }
 }
