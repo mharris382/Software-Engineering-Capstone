f90387f8df899361fae09b628bf4a94ec23c43f4
mAX <mharris382@gmail.com>
2022-03-10 01:16:49 -0500

    added unity events to health state

--- Elementals/Assets/Scripts/Damage/HealthState.cs
+++ Elementals/Assets/Scripts/Damage/HealthState.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public interface IHealth
 {
@@ -10,18 +11,32 @@ public interface IHealth
 
 public class HealthState : StatusValue, IHealth
 {
+    [Tooltip("Triggered when the entity gains health")]
+    public UnityEvent OnHealed;
+    
+    [Tooltip("Triggered when the entity loses health")]
+    public UnityEvent OnDamaged;
+    
+    [Tooltip("Triggered when the entity's health reaches zero")]
+    public UnityEvent OnKilled;
+    
     public bool isAlive => CurrentValue > 0;
 
     public void healHealth(float amount)
     {
+        var prev = CurrentValue;
         CurrentValue += amount;
+        if (prev < CurrentValue) OnHealed?.Invoke();
     }
 
     public void damageHealth(float amount)
     {
-        Debug.Log("Damage HEALTH");
+        if (isAlive == false) 
+            return;
+        var prev = CurrentValue;
         CurrentValue -= amount;
-        Debug.Log(CurrentValue);
+        if(prev > CurrentValue) OnDamaged?.Invoke();
+        if(!isAlive) OnKilled?.Invoke();
     }
 
     public void OnDestroy()
