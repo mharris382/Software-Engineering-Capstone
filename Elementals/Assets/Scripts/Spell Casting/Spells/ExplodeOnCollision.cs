060e11727049f1be70088ea7e5b3bf341d9fa89e
mAX <mharris382@gmail.com>
2022-03-16 19:47:58 -0400

    imported JMO cartoon fx pack for prototyping

---
+++ Elementals/Assets/Scripts/Spell Casting/Spells/ExplodeOnCollision.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Spell_Casting.Spells
+{
+    public class ExplodeOnCollision : MonoBehaviour
+    {
+        public GameObject explodePrefab;
+
+        public bool rotatePrefabToNormal;
+        
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Quaternion rotation = Quaternion.identity;
+            var pos = other.contacts[0].point;
+            if (rotatePrefabToNormal)
+            {
+                var dir = other.contacts[0].normal;
+                var ang = Vector2.SignedAngle(dir, Vector2.right);
+                rotation = Quaternion.Euler(0,0,ang);
+            }
+
+            var prefab = Instantiate(explodePrefab, pos, rotation);
+            Destroy(this.gameObject);
+        }
+    }
+}
