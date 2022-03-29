1dc30ae6f45a461549bfaf77e6137bf5665e1d8b
Max Harris <mharris382@gmail.com>
2022-03-29 19:50:57 -0400

    Bugfixes and movement improvements
    - fixed the player character animation in builds
    - mana now resets to full on death
    - thunder enemy shoots correctly in first level
    - added feedback button to main menu

--- Elementals/Assets/Scripts/Character Movement/MovementInputHandler.cs
+++ Elementals/Assets/Scripts/Character Movement/MovementInputHandler.cs
@@ -16,8 +16,8 @@ public class MovementInputHandler : MonoBehaviour
         _state.MovementInput.TryingToJump = Input.GetButton("Jump");
         _state.MovementInput.IsDoneJumping = Input.GetButtonUp("Jump");
         
-        var x = UnityEngine.Input.GetAxis("Horizontal");
-        var y = UnityEngine.Input.GetAxis("Vertical");
+        var x = UnityEngine.Input.GetAxisRaw("Horizontal");
+        var y = UnityEngine.Input.GetAxisRaw("Vertical");
         _state.MovementInput.MoveInput = new Vector2(x, y);
 
     }
--- Elementals/Assets/Scripts/Elements/ElementAnimationSwitcher.cs
+++ Elementals/Assets/Scripts/Elements/ElementAnimationSwitcher.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Mono.Cecil;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
@@ -9,13 +10,26 @@ public class ElementAnimationSwitcher : MonoBehaviour
 {
     public ElementContainer playerElement;
     [SerializeField] private RuntimeAnimatorController[] _controllers;
-
+    
     private Animator _anim;
     private RuntimeAnimatorController _defaultController;
     private Dictionary<Element, RuntimeAnimatorController> _elementControllers = new Dictionary<Element, RuntimeAnimatorController>();
-    
+    private List<RuntimeAnimatorController> racs;
     private void Awake()
     {
+        var anims = Resources.LoadAll<AnimatorOverrideController>("WizardAnimations");
+        var anims2 = Resources.LoadAll<RuntimeAnimatorController>("WizardAnimations");
+        racs = new List<RuntimeAnimatorController>();
+        foreach (var animatorOverrideController in anims)
+        {
+            racs.Add(animatorOverrideController);
+        }
+
+        foreach (var animatorController in anims2)
+        {
+            racs.Add(animatorController);
+        }
+        
         _anim = GetComponent<Animator>();
         _defaultController = _anim.runtimeAnimatorController;
         if (playerElement == null) playerElement = Resources.Load<ElementContainer>("PlayerElement");
@@ -24,7 +38,7 @@ public class ElementAnimationSwitcher : MonoBehaviour
         foreach (var element in elements)
         {
             var e = (Element) element;
-            var controller = _controllers.FirstOrDefault(t => t.name.Contains(element.ToString()));
+            var controller = racs.FirstOrDefault(t => t.name.Contains(element.ToString()));
             if(controller == null)Debug.LogError($"Couldn't find controller for element {element}",this);
             _elementControllers.Add(e, controller);
         }
@@ -45,9 +59,26 @@ public class ElementAnimationSwitcher : MonoBehaviour
 
     private void OnElementChanged(Element obj)
     {
-        if (_elementControllers.ContainsKey(obj) == false) 
+        if (_elementControllers.ContainsKey(obj) == false || _elementControllers[obj] == null)
+        {
             Debug.LogError($"Dictionary missing key: {obj}!", this);
-        
+            foreach (var runtimeAnimatorController in _controllers)
+            {
+                if (runtimeAnimatorController.name.Contains(obj.ToString()))
+                {
+                    _anim.runtimeAnimatorController = runtimeAnimatorController;
+                    return;
+                }
+            }
+            foreach (var acs in racs)
+            {
+                if (acs.name.Contains(obj.ToString()))
+                {
+                    _anim.runtimeAnimatorController = acs;
+                    return;
+                }
+            }
+        }
         var controller = _elementControllers[obj] != null ? _elementControllers[obj] : _defaultController;
         _anim.runtimeAnimatorController = controller;
     }
