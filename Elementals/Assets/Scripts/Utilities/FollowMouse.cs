27b9def81f0915358c64ae49a6fe5c2ebb3f3f00
mAX <mharris382@gmail.com>
2022-05-06 01:23:50 -0400

    added mouse texture

--- Elementals/Assets/Scripts/Utilities/FollowMouse.cs
+++ Elementals/Assets/Scripts/Utilities/FollowMouse.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 
 public class FollowMouse : MonoBehaviour
 {
+    public Texture2D targetCursor;
+    
     private void Update()
     {
         var mousePosition = Input.mousePosition;
         var mouseWP = Camera.main.ScreenToWorldPoint(mousePosition);
         transform.position = mouseWP;
+        Cursor.visible = true;
+        Cursor.SetCursor(targetCursor, new Vector2(0.5f, 0.5f), CursorMode.Auto);
     }
 }
